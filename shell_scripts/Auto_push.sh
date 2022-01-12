b1bb53e8af9f3ea61b47aeb95be1f62679376fdc
mahantheshmr <mahantheshmr.in@gmail.com>
2022-01-12 09:57:57 +0000

    Auto push jenkins Server linux Commit 12.01.2022 um 09:57

---
+++ shell_scripts/Auto_push.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+cd  #home path
+
+git remote add origin git@github.com:mahantheshmr/jenkins_linux_server.git
+
+git add *
+timestamp(){
+	   date +"%d.%m.%Y um %H:%M"
+   	   }
+git commit -am "Auto push jenkins Server linux Commit $(timestamp)"
+
+git push -u origin master
+
